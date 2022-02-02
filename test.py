47f8cc439540f87323d445b4a2322e179c63e126
kimjeongho <kimjeongho930829@gmail.com>
2022-02-02 14:40:30 +0900

    test project

---
+++ test.py
@@ -0,0 +1,8 @@
+import pyupbit
+
+access = ""          # 본인 값으로 변경
+secret = ""          # 본인 값으로 변경
+upbit = pyupbit.Upbit(access, secret)
+
+print(upbit.get_balance("KRW"))         # 보유 현금 조회
+print(upbit.get_balance("KRW-ETH"))     # KRW-ETH 조회
